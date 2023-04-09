668f1060496f89e676943dea4b4a2d7e8a5b2860
klemo <matuskoklemo@gmail.com>
2023-04-09 16:10:25 +0200

    refactor: enhance solution for 1.2. check permutation

--- app/src/main/java/ctci/exercises/_1_arrays_and_strings/check_permutation/CheckPermutation.java
+++ app/src/main/java/ctci/exercises/_1_arrays_and_strings/check_permutation/CheckPermutation.java
@@ -1,18 +1,22 @@
 package ctci.exercises._1_arrays_and_strings.check_permutation;
 
 public class CheckPermutation {
+    /*
+     * This solution compares if count of all characters are same in strings
+     * O(1) space approach would use sorting and then compare if sorted strings are equal, this would cost
+     * approximately O(nlog(n)) time as a tradeoff
+     */
     public boolean isPermutation(String str1, String str2) {
         if (str1.length() != str2.length()) return false;
-        int[] freq1 = new int['z'-'a'+1];
-        int[] freq2 = new int['z'-'a'+1];
+        int[] freq = new int['z'-'a'+1];
 
         for (int i = 0; i < str1.length(); i++) {
-            freq1[str1.charAt(i)-'a']++;
-            freq2[str2.charAt(i)-'a']++;
+            freq[str1.charAt(i)-'a']++;
+            freq[str2.charAt(i)-'a']--;
         }
 
-        for (int i = 0; i < freq1.length; i++) {
-            if (freq1[i] != freq2[i]) return false;
+        for (int c : freq) {
+            if (c != 0) return false;
         }
 
         return true;
