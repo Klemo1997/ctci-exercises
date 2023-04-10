9c5ae48a5ccbaccdb978c29a02bccfefbd15df08
klemo <matuskoklemo@gmail.com>
2023-04-10 17:11:39 +0200

    fix: implement correct solution for 1.4. Palindrome Permutation

--- app/src/test/java/ctci/exercises/_1_arrays_and_strings/PalindromePermutationTest.java
+++ app/src/test/java/ctci/exercises/_1_arrays_and_strings/PalindromePermutationTest.java
@@ -12,19 +12,19 @@ import static org.junit.jupiter.api.Assertions.assertSame;
 public class PalindromePermutationTest {
     @ParameterizedTest
     @MethodSource("providePalindromePermutation")
-    public void testIsPalindromePermutation(boolean expected, String str1, String str2) {
-        assertSame(expected, new PalindromePermutation().isPalindromePermutation(str1, str2));
+    public void testIsPalindromePermutation(boolean expected, String s) {
+        assertSame(expected, new PalindromePermutation().isPalindromePermutation(s));
     }
 
     private static Stream<Arguments> providePalindromePermutation() {
         return Stream.of(
-            Arguments.of(true, "", ""),
-            Arguments.of(false, "a", "b"),
-            Arguments.of(true, "a", "a"),
-            Arguments.of(true, "tact coa", "taco cat"),
-            Arguments.of(true, "ab cd ef", "fecdba  "),
-            Arguments.of(false, "ab cd efs", "fecdba  x"),
-            Arguments.of(false, "abcd", "cbac")
+            Arguments.of(true, ""),
+            Arguments.of(true, "a"),
+            Arguments.of(false, "ab"),
+            Arguments.of(true, "aab"),
+            Arguments.of(true, "aabb"),
+            Arguments.of(false, "aaabbb"),
+            Arguments.of(true, "tact coa")
         );
     }
 }
