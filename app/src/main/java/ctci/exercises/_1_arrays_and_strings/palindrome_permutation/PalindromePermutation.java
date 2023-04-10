964a55fe84f6739c1182742a6914767bcdf60407
klemo <matuskoklemo@gmail.com>
2023-04-10 17:22:40 +0200

    refactor: enhance fixed solution for 1.4. Palindrome Permutation

--- app/src/main/java/ctci/exercises/_1_arrays_and_strings/palindrome_permutation/PalindromePermutation.java
+++ app/src/main/java/ctci/exercises/_1_arrays_and_strings/palindrome_permutation/PalindromePermutation.java
@@ -2,21 +2,23 @@ package ctci.exercises._1_arrays_and_strings.palindrome_permutation;
 
 public class PalindromePermutation {
     /*
-     * We increase the size of freq array to support ASCII set of first 128 characters.
-     * Then it is all about counting the occurrences of characters and comparing them between strings.
-     * If they do have same number of all characters, they are palindrome permutations of each other.
+     * Solution is all about defining what is a palindrome and permutation
+     * Palindrome - string that is same forwards and backwards
+     * Permutation - same string, but with letters in different order
+     * So we need to check if we can order letters somehow so the input will become palindrome
+     *
+     * The conditions are that all character counts but one (or zero) must be even. So we'll allow at most 1 odd letter
      */
     public boolean isPalindromePermutation(String s) {
-        int[] freq = new int['z'-'a'+1];
+        int bits = 0;
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == ' ') continue;
-            freq[s.charAt(i)-'a']++;
+            // XOR is nice way to flip our bits
+            if (s.charAt(i) != ' ') bits ^= (1 << s.charAt(i)-'a');
         }
         int oddCount = 0;
-        for (int c: freq) {
-            if (c % 2 == 1) oddCount++;
-            if (oddCount > 1) return false;
+        for (int i = 0; i < 'z'-'a'+1; i++) {
+            if ((bits & (1 << i)) != 0) oddCount++;
         }
-        return true;
+        return oddCount <= 1;
     }
 }
