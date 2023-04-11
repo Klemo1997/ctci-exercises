442436f118605d24f207f288065d5caccc4be2a6
klemo <matuskoklemo@gmail.com>
2023-04-11 20:37:19 +0200

    refactor: enhance solution for 1.6. String Compression - inspired by solutions

--- app/src/main/java/ctci/exercises/_1_arrays_and_strings/string_compression/StringCompression.java
+++ app/src/main/java/ctci/exercises/_1_arrays_and_strings/string_compression/StringCompression.java
@@ -2,22 +2,16 @@ package ctci.exercises._1_arrays_and_strings.string_compression;
 
 public class StringCompression {
     public String compressString(String str) {
-        if (str.length() == 0) return str;
         StringBuilder compressed = new StringBuilder();
-        compressed.append(str.charAt(0));
-        int ctr = 1;
-        for (int i = 1; i < str.length(); i++) {
-            if (str.charAt(i) == str.charAt(i-1)) {
-                ctr++;
-                continue;
+        int ctr = 0;
+        for (int i = 0; i < str.length(); i++) {
+            ctr++;
+            if (i + 1 == str.length() || str.charAt(i) != str.charAt(i+1)) {
+                compressed.append(str.charAt(i)).append(ctr);
+                ctr = 0;
             }
-            compressed.append(ctr);
-            compressed.append(str.charAt(i));
-            ctr = 1;
             if (compressed.length() >= str.length()) return str;
         }
-        compressed.append(ctr);
-        String out = compressed.toString();
-        return out.length() < str.length() ? out : str;
+        return compressed.toString();
     }
 }
